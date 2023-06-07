7cdd5deded0fee81800606f2762c7bb57fe057af
James Oladiran <oluwajamie@gmail.com>
2023-06-07 01:40:31 -0700

    Advanced task done

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,7 +7,7 @@
  * @s2: string with wild character
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * On error, -1 is returned, and error is set appropriately.
  */
 int wildcmp(char *s1, char *s2)
 {
