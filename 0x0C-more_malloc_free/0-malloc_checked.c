90cabd10a743ded4de86237e6eba054e3f2078a2
James Oladiran <oluwajamie@gmail.com>
2023-06-15 15:20:56 -0700

    Trust

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,19 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * malloc_checked - function that allocates memory using malloc
- * @b: size of the int memory to be allocated
+ * @b: size of the memory to be allocated
  *
  * Return: pointer to the allocated memory
  */
  
 void *malloc_checked(unsigned int b)
 {
-	void *block;
+	void *block = malloc(b);
 
-	block = malloc(b);
 	if (block == NULL)
 		exit(98);
+
 	return (block);
 }
