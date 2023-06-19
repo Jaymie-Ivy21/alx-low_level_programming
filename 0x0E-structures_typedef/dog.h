cd911d3459a618c5ea1d66134cf50c92e1a2a8d8
James Oladiran <oluwajamie@gmail.com>
2023-06-19 11:19:08 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Structure
+ * @name: type element
+ * @age: type element
+ * @owner: type element
+ * Desc: Define a new type
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/**
+ * typedef struct dog_t dog - New structure
+ * Desc: A new type dog_t as the new name for the type struct dog
+ */
+typedef struct dog dog_t;
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
